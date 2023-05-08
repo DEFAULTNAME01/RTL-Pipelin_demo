cc126baebcc2cba50476442d5bf6ac96651de754
DEFAULTNAME01 <48400142+DEFAULTNAME01@users.noreply.github.com>
2023-05-08 12:52:27 +0100

    Update main.c
    断点运行可以，持续运行有问题

--- RTLpipelinedemo/main.c
+++ RTLpipelinedemo/main.c
@@ -2,97 +2,129 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include <stdbool.h>
 #include <time.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#define ARRAY_SIZE 5
+#define INPUT_SIZE 25
 
 typedef struct {
-    int data[5];
+    int data[ARRAY_SIZE];
     bool valid;
 } DataStructure;
 
-DataStructure pipeline_register;
-sem_t sem_A, sem_B;
+DataStructure A_register, B_register, C_register;
+DataStructure A_iregister, B_iregister, C_iregister;
+int input_data[INPUT_SIZE] = { 2, 3, 18, 9, 1, 34, 52, 11, 26, 8, 12, 44, 8, 97, 10, 22, 31, 4, 6, 48, 5, 0, 13, 49, 7 };
 bool continuous_run = false;
 bool program_terminated = false;
+sem_t myclk;
 
 void* function_A(void* args) {
-    while (continuous_run) {
-        sem_wait(&sem_A);
-        for (int i = 0; i < 5; ++i) {
-            pipeline_register.data[i] = rand() % 100;
+    int index = 0;
+    while (!program_terminated) {
+        
+
+        for (int i = 0; i < ARRAY_SIZE; ++i) {
+            A_iregister.data[i] = input_data[index % INPUT_SIZE];
+            index++;
         }
-        pipeline_register.valid = true;
-        printf("Data in pipeline is now valid\n");
-        sem_post(&sem_B);
+        sem_wait(&myclk);
+        A_register = A_iregister;
+        A_register.valid = true;
+        printf("First element of structure at \nA:%d B:%d C:%d\n", A_register.data[0], B_register.data[0], C_register.data[0]);
     }
     return NULL;
 }
 
 void* function_B(void* args) {
-    while (continuous_run) {
-        while (!pipeline_register.valid) {
-            // Wait for data to become valid
+    while (!program_terminated) {
+       
+
+        if (!A_register.valid) {
+            continue;
         }
-        printf("First element of structure at B: %d\n", pipeline_register.data[0]);
-        pipeline_register.valid = false;
-        sem_post(&sem_A);
+        sem_wait(&myclk);
+        for (int i = 0; i < ARRAY_SIZE; ++i) {
+            B_iregister.data[i] = A_register.data[i] - 1;
+        }
+        A_register.valid = false;
+      
+        
+
+        B_register = B_iregister;
+        B_register.valid = true;
     }
     return NULL;
 }
 
-void* control_function(void* arg) {
-    while (true) {
-        int ch = getchar();
-        if (ch == 's' || ch == 'S') {
-            continuous_run = false;
-            program_terminated = true;
-            break;
+void* function_C(void* args) {
+    while (!program_terminated) {
+       
+
+        if (!B_register.valid) {
+            continue;
         }
-        if (ch == 'p' || ch == 'P') {
+        sem_wait(&myclk);
+        for (int i = 0; i < ARRAY_SIZE; ++i) {
+            C_iregister.data[i] = B_register.data[i] - 1;
+        }
+        B_register.valid = false;
+        
+        C_register = C_iregister;
+        C_register.valid = true;
+    }
+    return NULL;
+}
+
+void* control_and_clocks(void* arg) {
+    int ch;
+
+    while (!program_terminated) {
+        ch = getchar();
+
+        if (ch == 's' || ch == 'S') {
+            program_terminated = true;
+        } else if (ch == 'p' || ch == 'P') {
             continuous_run = true;
-            sem_post(&sem_A);
+        } else if (ch == 'l' || ch == 'L') {
+            continuous_run = false;
+        }
+
+        if (continuous_run) {
+            usleep(100);
+            sem_post(&myclk);
+        } else if (ch == '\n') {
+            sem_post(&myclk);
         }
     }
     return NULL;
 }
+
 
 int main() {
-    pthread_t thread_A, thread_B, thread_control;
-    int ch;
-    time_t start_time = clock();
+    pthread_t thread_A, thread_B, thread_C, control_and_clocks_thread;
 
-    // Initialize semaphores
-    sem_init(&sem_A, 0, 0);
-    sem_init(&sem_B, 0, 0);
-
-    // Wait for user input to start the program
-    while (true) {
-        ch = getchar();
-        if (ch == 'p' || ch == 'P') {
-            continuous_run = true;
-            sem_post(&sem_A);
-            break;
-        }
-    }
+    // Initialize semaphore
+    sem_init(&myclk, 0, 0);
 
     // Create threads
     pthread_create(&thread_A, NULL, function_A, NULL);
     pthread_create(&thread_B, NULL, function_B, NULL);
-    pthread_create(&thread_control, NULL, control_function, NULL);
+    pthread_create(&thread_C, NULL, function_C, NULL);
+    pthread_create(&control_and_clocks_thread, NULL, control_and_clocks, NULL);
 
     // Wait for threads to finish
     pthread_join(thread_A, NULL);
     pthread_join(thread_B, NULL);
-    pthread_join(thread_control, NULL);
+    pthread_join(thread_C, NULL);
+    pthread_join(control_and_clocks_thread, NULL);
 
-    // Record end time and calculate elapsed time
-    time_t end_time = clock();
-    double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC * 1000.0;
-
-    // Display results
-    printf("running_times : %.4f ms\n", elapsed_time);
-    sem_destroy(&sem_A);
-    sem_destroy(&sem_B);
+    // Destroy semaphore
+    sem_destroy(&myclk);
 
     return 0;
 }
+
+
